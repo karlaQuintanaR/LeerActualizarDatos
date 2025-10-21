d3761de3a2a0e2aa44410f168c4a4cc33bb060c3
karlaQuintanaR <s22120215@alumnos.itsur.edu.mx>
2025-10-21 11:00:28 -0600

    Se agrego el método de actualización y eliminación a ItemDao

--- app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -26,6 +26,12 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ItemDao {
+    @Query("SELECT * FROM items ORDER BY name ASC")
+    fun getAllItems(): Flow<List<Item>>
+
+    @Query("SELECT * FROM items WHERE id = :id")
+    fun getItem(id: Int): Flow<Item?>
+
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(item: Item)
 
@@ -34,11 +40,6 @@ interface ItemDao {
 
     @Delete
     suspend fun delete(item: Item)
-
-    @Query("SELECT * from items WHERE id = :id")
-    fun getItem(id: Int): Flow<Item>
-
-    @Query("SELECT * from items ORDER BY name ASC")
-    fun getAllItems(): Flow<List<Item>>
 }
+
 
